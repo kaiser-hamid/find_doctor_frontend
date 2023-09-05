3f7846c09628509290d467458bc59ce1ae41fa1b
Kaiser <khtusher.mail@gmail.com>
2023-09-05 23:44:35 +0600

    Doctor details page design

--- src/app/[locale]/(general)/doctors/[id]/BannerSection.js
+++ src/app/[locale]/(general)/doctors/[id]/BannerSection.js
@@ -1,11 +1,19 @@
+import Image from "next/image";
+
 export default () => {
   return (
-    <section className="pt-36 pb-24 bg-slate-100">
+    <section className="pt-36 bg-teal-100 bg-cover bg-no-repeat bg-center bg-[url('/assets/images/photos/5.jpg')]">
       <div className="container">
-        <div className="text-center max-w-2xl mx-auto">
-          <h2 className="md:text-5xl/tight text-3xl font-semibold mb-5 drop-shadow">
-            Dr. Haripada Dhar
-          </h2>
+        <div className="flex justify-center">
+          <div className="flex justify-center items-center relative -bottom-28 h-56 w-56 rounded-full overflow-hidden border-2 border-slate-300 bg-slate-100">
+            <Image
+              src="/assets/images/hero/portfolio1.png"
+              className="h-full w-full"
+              alt="Doctor image"
+              width={170}
+              height={170}
+            />
+          </div>
         </div>
       </div>
     </section>
