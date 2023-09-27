8b3d63fa240c0ae237d7c3d691f936e012397e9c
Kaiser <khtusher.mail@gmail.com>
2023-09-28 00:39:51 +0600

    remove commented code from dr details

--- src/app/[locale]/(general)/doctors/[id]/AboutDoctor.js
+++ src/app/[locale]/(general)/doctors/[id]/AboutDoctor.js
@@ -4,22 +4,6 @@ import { FaStar, FaUser, FaUserAlt } from "react-icons/fa";
 export default ({ data }) => {
   const t = useTranslations("doctor");
   const locale = useLocale();
-  // return (
-  //   <section className="pb-20">
-  //     <div className="container">
-  //       <div className="mt-8">
-  //         <div className="text-justify">
-  //           <h3 className="text-2xl">
-  //             {t("about doctor", { name: data.name[locale] })}
-  //           </h3>
-  //           <p className="text-slate-800 mt-4">
-  //             {data.about_doctor && data.about_doctor[locale]}
-  //           </p>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </section>
-  // );
   return (
     <section className="pb-20" data-aos="fade-up">
       <div className="container relative">
